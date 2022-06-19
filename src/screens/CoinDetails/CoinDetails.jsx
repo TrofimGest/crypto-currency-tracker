13cbe673b2597fdfe13ca700d5004a1a2dc51a76
TrofimGest <gubaoslika@gmail.com>
2022-06-20 00:27:14 +0300

    feat: add basic example of the chart

--- src/screens/CoinDetails/CoinDetails.jsx
+++ src/screens/CoinDetails/CoinDetails.jsx
@@ -1,5 +1,11 @@
-import { View, Text, Image } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import React from 'react';
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+} from '@rainbow-me/animated-charts';
+
 import Coin from '../../../assets/data/crypto.json';
 import CoinHeader from '../../components/CoinHeader/CoinHeader';
 import Price from '../../components/Price/Price';
@@ -17,6 +23,8 @@ const CoinDetails = () => {
     },
   } = Coin;
 
+  const screenWidth = Dimensions.get('window').width;
+
   return (
     <View style={{ width: '100%', paddingHorizontal: 10 }}>
       <CoinHeader
@@ -29,6 +37,19 @@ const CoinDetails = () => {
         currentPrice={current_price}
         priceChange={price_change_percentage_24h}
       />
+      <ChartPathProvider
+        data={{
+          points: prices.map(([x, y]) => ({ x, y })),
+          smoothingStrategy: 'bezier',
+        }}
+      >
+        <ChartPath
+          height={screenWidth / 2}
+          stroke='yellow'
+          width={screenWidth}
+        />
+        <ChartDot style={{ backgroundColor: 'blue' }} />
+      </ChartPathProvider>
     </View>
   );
 };
