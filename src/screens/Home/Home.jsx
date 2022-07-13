78036a46df8b4fd7d8384435edf86a4534d571fd
TrofimGest <gubaoslika@gmail.com>
2022-07-13 23:22:03 +0300

    feat: get list of coins from api

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,12 +1,26 @@
 import { View, Text, FlatList } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CoinCard from '../../components/CoinCard/CoinCard';
-import cryptocurrencies from '../../../assets/data/cryptocurrencies.json';
+import { getMarketData } from '../../services/api';
 
 const Home = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <FlatList
-      data={cryptocurrencies}
+      data={coins}
       renderItem={({ item }) => <CoinCard marketCoin={item} />}
     />
   );
