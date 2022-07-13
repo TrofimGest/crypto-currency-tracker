1a866ce6157b2f93063293cc65ea5f0bda95c892
TrofimGest <gubaoslika@gmail.com>
2022-07-13 23:59:02 +0300

    feat: add refresh control to home screen

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,13 +1,27 @@
-import { View, Text, FlatList } from 'react-native';
+import { FlatList, RefreshControl } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import CoinCard from '../../components/CoinCard/CoinCard';
 import { getMarketData } from '../../services/api';
+import { combineTransition } from 'react-native-reanimated';
 
 const Home = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchCoins = async () => {
+  const fetchCoins = async (page) => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const coinsData = await getMarketData(page);
+    setCoins((existingCoins) => [...existingCoins, ...coinsData]);
+    setLoading(false);
+  };
+
+  const refetchCoins = async () => {
+    if (loading) {
+      return;
+    }
     setLoading(true);
     const coinsData = await getMarketData();
     setCoins(coinsData);
@@ -22,6 +36,15 @@ const Home = () => {
     <FlatList
       data={coins}
       renderItem={({ item }) => <CoinCard marketCoin={item} />}
+      onEndReached={() => fetchCoins(coins.length / 50 + 1)}
+      keyExtractor={(item) => item.id}
+      refreshControl={
+        <RefreshControl
+          refreshing={loading}
+          tintColor='white'
+          onRefresh={refetchCoins}
+        />
+      }
     />
   );
 };
